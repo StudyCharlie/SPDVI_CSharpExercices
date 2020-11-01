194ce9a12e5df181818606fcdc3e48a1e64ecf93
Carlos Fernandez <carlosfernandez@paucasesnovescifp.cat>
2020-11-01 15:59:24 +0100

    We done our first commit to GitHub.
    We ordering our projects in a new repository, started with some Exercices about Lists with Strings and Lists with objects type PersonModel.
    
    Im going to do some folders just for save some ressources, looking for a good base in C#, dont want to complete all Exercices without understand it.

---
+++ SPDVI_CSharpExercices/SPDVI_CSharpExercices/ForEachLists/PersonModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForEachLists
+{
+    class PersonModel
+    {
+        String firstName;
+        String lastName;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+        public PersonModel(String firstName, String lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+
+}
---
+++ SPDVI_CSharpExercices/SPDVI_CSharpExercices/ForEachLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEachLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> names = new List<String>();
+            names.Add("John");
+            names.Add("Mary");
+            names.Add("Sue");
+            names.Add("Greg");
+            names.Add("Yolanda");
+            names.Add("Jose");
+            names.Add("Bill");
+
+            foreach(var name in names)
+            {
+                Console.WriteLine("Hello "+name);
+            }
+
+            List<PersonModel> people = new List<PersonModel>();
+
+            PersonModel one = new PersonModel("Tim ","Corey");
+            PersonModel two = new PersonModel("Bill ", "Mccoy");
+            PersonModel three = new PersonModel("Mary ", "Jones");
+            PersonModel four = new PersonModel("Sue ", "Smith");
+
+            people.Add(one);
+            people.Add(two);
+            people.Add(three);
+            people.Add(four);
+
+            Console.WriteLine("");
+            foreach (var person in people)
+            {
+                Console.WriteLine("Hello " + person.FirstName + "" + person.LastName);
+            }
+        }
+    }
+}
