0bbd2ad7f2223d36f2a214e41516f7887a872ac1
Carlos Fernandez <carlosfernandez@paucasesnovescifp.cat>
2020-11-02 15:04:09 +0100

    We finished the Exercice about Text File challenge.
    Needed to see the solution video because I get some problems with the Exercice.

---
+++ SPDVI_CSharpExercices/SPDVI_CSharpExercices/TextFileChallenge/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace TextFileChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.isAliveLabel = new System.Windows.Forms.Label();
+            this.addUserButton = new System.Windows.Forms.Button();
+            this.saveListButton = new System.Windows.Forms.Button();
+            this.isAliveCheckBox = new System.Windows.Forms.CheckBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.ageNumeric = new System.Windows.Forms.NumericUpDown();
+            this.usersListBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ageNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.25F);
+            this.label1.Location = new System.Drawing.Point(27, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text File Challenge";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.firstNameLabel.Location = new System.Drawing.Point(31, 134);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(92, 20);
+            this.firstNameLabel.TabIndex = 1;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lastNameLabel.Location = new System.Drawing.Point(31, 188);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(91, 20);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.ageLabel.Location = new System.Drawing.Point(31, 243);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(38, 20);
+            this.ageLabel.TabIndex = 3;
+            this.ageLabel.Text = "Age";
+            // 
+            // isAliveLabel
+            // 
+            this.isAliveLabel.AutoSize = true;
+            this.isAliveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.isAliveLabel.Location = new System.Drawing.Point(31, 290);
+            this.isAliveLabel.Name = "isAliveLabel";
+            this.isAliveLabel.Size = new System.Drawing.Size(63, 20);
+            this.isAliveLabel.TabIndex = 4;
+            this.isAliveLabel.Text = "Is Alive";
+            // 
+            // addUserButton
+            // 
+            this.addUserButton.Location = new System.Drawing.Point(35, 348);
+            this.addUserButton.Name = "addUserButton";
+            this.addUserButton.Size = new System.Drawing.Size(263, 50);
+            this.addUserButton.TabIndex = 5;
+            this.addUserButton.Text = "Add User";
+            this.addUserButton.UseVisualStyleBackColor = true;
+            this.addUserButton.Click += new System.EventHandler(this.addUserButton_Click);
+            // 
+            // saveListButton
+            // 
+            this.saveListButton.Location = new System.Drawing.Point(35, 423);
+            this.saveListButton.Name = "saveListButton";
+            this.saveListButton.Size = new System.Drawing.Size(263, 54);
+            this.saveListButton.TabIndex = 6;
+            this.saveListButton.Text = "Save List";
+            this.saveListButton.UseVisualStyleBackColor = true;
+            this.saveListButton.Click += new System.EventHandler(this.saveListButton_Click);
+            // 
+            // isAliveCheckBox
+            // 
+            this.isAliveCheckBox.AutoSize = true;
+            this.isAliveCheckBox.Location = new System.Drawing.Point(141, 296);
+            this.isAliveCheckBox.Name = "isAliveCheckBox";
+            this.isAliveCheckBox.Size = new System.Drawing.Size(15, 14);
+            this.isAliveCheckBox.TabIndex = 7;
+            this.isAliveCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(141, 136);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(157, 20);
+            this.firstNameTextBox.TabIndex = 8;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(141, 188);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(157, 20);
+            this.lastNameTextBox.TabIndex = 9;
+            // 
+            // ageNumeric
+            // 
+            this.ageNumeric.Location = new System.Drawing.Point(141, 246);
+            this.ageNumeric.Name = "ageNumeric";
+            this.ageNumeric.Size = new System.Drawing.Size(56, 20);
+            this.ageNumeric.TabIndex = 10;
+            // 
+            // usersListBox
+            // 
+            this.usersListBox.FormattingEnabled = true;
+            this.usersListBox.Items.AddRange(new object[] {
+            ""});
+            this.usersListBox.Location = new System.Drawing.Point(362, 136);
+            this.usersListBox.Name = "usersListBox";
+            this.usersListBox.Size = new System.Drawing.Size(425, 342);
+            this.usersListBox.TabIndex = 11;
+            this.usersListBox.SelectedIndexChanged += new System.EventHandler(this.usersListBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 514);
+            this.Controls.Add(this.usersListBox);
+            this.Controls.Add(this.ageNumeric);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.isAliveCheckBox);
+            this.Controls.Add(this.saveListButton);
+            this.Controls.Add(this.addUserButton);
+            this.Controls.Add(this.isAliveLabel);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ageNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Label isAliveLabel;
+        private System.Windows.Forms.Button addUserButton;
+        private System.Windows.Forms.Button saveListButton;
+        private System.Windows.Forms.CheckBox isAliveCheckBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.NumericUpDown ageNumeric;
+        private System.Windows.Forms.ListBox usersListBox;
+    }
+}
+
---
+++ SPDVI_CSharpExercices/SPDVI_CSharpExercices/TextFileDataAccess/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextFileDataAccess
+{
+    public class Person
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+
+        public String url { get; set; }
+
+        
+    }
+}
